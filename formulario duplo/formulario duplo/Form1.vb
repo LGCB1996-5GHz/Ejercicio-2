55d9d1242678a946fa97f6cbcffc6d78c3a7cf72
Leonel Gonzalez <110871548+LGCB1996-5GHz@users.noreply.github.com>
2022-08-29 20:48:43 -0300

    Update Form1.vb

--- formulario duplo/formulario duplo/Form1.vb
+++ formulario duplo/formulario duplo/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Form2.Show()
+        Form2.SetDesktopLocation(300, 70)
 
     End Sub
 
